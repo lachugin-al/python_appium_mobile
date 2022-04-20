22a338831255400bc4fc0793b7b995c3507288c3
lachugin <anton.lachugin.l@gmail.com>
2022-04-20 14:00:34 +0300

    refactor

--- src/pages/android/km_page.py
+++ src/pages/android/km_page.py
@@ -12,7 +12,8 @@ class KMPage(Driver):
     # КМ товара
     pricesPriceBnplTextView = (MobileBy.ID, "ru.beru.android.qa:id/pricesPriceBnplTextView")  # или частями около цены
     productMainCartButton = (MobileBy.ID, "ru.beru.android.qa:id/productMainCartButton")  # кнопка Добавить в корзину
-    cartButtonProgressButton = (MobileBy.ID, "ru.beru.android.qa:id/cartButtonProgressButton")  # значение кнопки Добавить в корзину
+    cartButtonProgressButton = (
+        MobileBy.ID, "ru.beru.android.qa:id/cartButtonProgressButton")  # значение кнопки Добавить в корзину
     cartMinusButton = (MobileBy.ID, "ru.beru.android.qa:id/cartMinusButton")  # -
     cartPlusButton = (MobileBy.ID, "ru.beru.android.qa:id/cartPlusButton")  # +
 
@@ -21,28 +22,27 @@ class KMPage(Driver):
     bnplBlockInitSum = (MobileBy.ID, "ru.beru.android.qa:id/bnplBlockInitSum")  # * сегодня
     bnplBlockMonthSum = (MobileBy.ID, "ru.beru.android.qa:id/bnplBlockMonthSum")  # и * потом, без переплат
     proceedBnpl = (MobileBy.ID, "ru.beru.android.qa:id/proceedBnpl")  # оформить
-    bnplTableView = (MobileBy.ID, "ru.beru.android.qa:id/bnplTableView")  # график
-    firstPayment = (MobileBy.ID, "ru.beru.android.qa:id/firstPayment")  # колбаска1
-    secondPayment = (MobileBy.ID, "ru.beru.android.qa:id/secondPayment")  # колбаска2
-    thirdPayment = (MobileBy.ID, "ru.beru.android.qa:id/third_payment")  # колбаска3
-    firthPayment = (MobileBy.ID, "ru.beru.android.qa:id/firth_payment")  # колбаска4
-    firstDateTextView = (MobileBy.ID, "ru.beru.android.qa:id/firstDateTextView")  # дата 1 платежа
-    secondDateTextView = (MobileBy.ID, "ru.beru.android.qa:id/secondDateTextView")  # дата 2 платежа
-    thirdDateTextView = (MobileBy.ID, "ru.beru.android.qa:id/thirdDateTextView")  # дата 3 платежа
-    fourthDateTextView = (MobileBy.ID, "ru.beru.android.qa:id/fourthDateTextView")  # дата 4 платежа
-    firstAmountTextView = (MobileBy.ID, "ru.beru.android.qa:id/firstAmountTextView")  # сумма 1 платежа
-    secondAmountTextView = (MobileBy.ID, "ru.beru.android.qa:id/secondAmountTextView")  # сумма 2 платежа
-    thirdAmountTextView = (MobileBy.ID, "ru.beru.android.qa:id/thirdAmountTextView")  # сумма 3 платежа
-    fourthAmountTextView = (MobileBy.ID, "ru.beru.android.qa:id/fourthAmountTextView")  # сумма 4 платежа
+    bnplPaymentsTableView = (MobileBy.ID, "ru.beru.android.qa:id/bnplPaymentsTableView")  # график
+    commissionTextView = (MobileBy.ID, "ru.beru.android.qa:id/commissionTextView")  # Без переплат
+
+    firstTableItem = (MobileBy.ID, "ru.beru.android.qa:id/firstTableItem")  # первый блок графика
+    paymentPeriodIcon = (MobileBy.ID, "ru.beru.android.qa:id/paymentPeriodIcon")  # колбаска
+    dateTextView = (MobileBy.ID, "ru.beru.android.qa:id/dateTextView")  # дата
+    amountTextView = (MobileBy.ID, "ru.beru.android.qa:id/amountTextView")  # сумма платежа
+
+    secondTableItem = (MobileBy.ID, "ru.beru.android.qa:id/secondTableItem")  # второй блок графика
+    thirdTableItem = (MobileBy.ID, "ru.beru.android.qa:id/thirdTableItem")  # третий блок графика
+    fourthTableItem = (MobileBy.ID, "ru.beru.android.qa:id/fourthTableItem")  # четвертый блок графика
+
     moreInfoTextView = (MobileBy.ID, "ru.beru.android.qa:id/moreInfoTextView")  # подробнее
     # moreInfoTextViewAfterOpen = (MobileBy.XPATH, "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
     moreInfoTextViewAfterOpen = (
-    MobileBy.XPATH, "//android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
+        MobileBy.XPATH, "//android.widget.LinearLayout/android.view.ViewGroup/android.widget.TextView")
 
     # Виджет Рассрочки
     offerInstallmentsBlock = (MobileBy.ID, "ru.beru.android.qa:id/offerInstallmentsBlock")  # виджет
     installmentsTitleTextView = (
-    MobileBy.ID, "ru.beru.android.qa:id/installmentsTitleTextView")  # Рассрочка от Тинькофф
+        MobileBy.ID, "ru.beru.android.qa:id/installmentsTitleTextView")  # Рассрочка от Тинькофф
     installmentsTermView = (MobileBy.ID, "ru.beru.android.qa:id/installmentsTermView")  # блок на 3 мес = ... ₽/мес
     onePeriodTextView = (MobileBy.ID, "ru.beru.android.qa:id/onePeriodTextView")  # 3 мес
     monthlyPayment = (MobileBy.ID, "ru.beru.android.qa:id/monthlyPayment")  # ... ₽/мес
@@ -85,9 +85,9 @@ class KMPage(Driver):
     # Виджет Кредита
     offerCreditBlock = (MobileBy.ID, "ru.beru.android.qa:id/offerCreditBlock")  # виджет
     cartTinkoffCreditBlockTitle = (
-    MobileBy.ID, "ru.beru.android.qa:id/cartTinkoffCreditBlockTitle")  # Кредит от Тинькофф
+        MobileBy.ID, "ru.beru.android.qa:id/cartTinkoffCreditBlockTitle")  # Кредит от Тинькофф
     cartTinkoffCreditBlockMonthPayment = (
-    MobileBy.ID, "ru.beru.android.qa:id/cartTinkoffCreditBlockMonthPayment")  # от ... ₽ / мес.
+        MobileBy.ID, "ru.beru.android.qa:id/cartTinkoffCreditBlockMonthPayment")  # от ... ₽ / мес.
     proceedCredit = (MobileBy.ID, "ru.beru.android.qa:id/proceed_credit")  # Оформить
 
     # Хэддер
@@ -95,7 +95,7 @@ class KMPage(Driver):
     pricesBasePriceView = (MobileBy.ID, "ru.beru.android.qa:id/pricesBasePriceView")
     pricesPriceView = (MobileBy.ID, "ru.beru.android.qa:id/pricesPriceView")
     creditPriceView = (
-    MobileBy.ID, "ru.beru.android.qa:id/creditPriceView")  # от ... ₽ / мес. в компактном оффере [@index = 1]
+        MobileBy.ID, "ru.beru.android.qa:id/creditPriceView")  # от ... ₽ / мес. в компактном оффере [@index = 1]
     cartCounterButton = (MobileBy.ID, "ru.beru.android.qa:id/cartCounterButton")
 
     def __init__(self, driver):
